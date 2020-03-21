1de7d974a7183787a1479ee7357b4fb19669e232
ShuwenHe <1201220707@pku.edu.cn>
2020-03-22 00:49:36 +0800

    add

--- code/hello/hello.go
+++ code/hello/hello.go
@@ -3,6 +3,7 @@ package main
 // 1、首先我们引入了包github.com/astaxie/beego,我们知道Go语言里面引入包会深度优先的去执行引入包的初始化(变量和init函数，更多)，beego包中会初始化一个BeeAPP的应用，初始化一些参数。
 import "github.com/astaxie/beego" 
 
+// 2、定义Controller，这里我们定义了一个struct为MainController，充分利用了Go语言的组合的概念，匿名包含了beego.Controller，这样我们的MainController就拥有了beego.Controller的所有方法。
 type MainController struct{
 	beego.Controller
 }
