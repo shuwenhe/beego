da5757f65db8e45a86005cf15e143df143057f16
ShuwenHe <1201220707@pku.edu.cn>
2020-03-22 00:48:53 +0800

    add import 1

--- code/hello/hello.go
+++ code/hello/hello.go
@@ -1,6 +1,7 @@
 package main
 
-import "github.com/astaxie/beego"
+// 1、首先我们引入了包github.com/astaxie/beego,我们知道Go语言里面引入包会深度优先的去执行引入包的初始化(变量和init函数，更多)，beego包中会初始化一个BeeAPP的应用，初始化一些参数。
+import "github.com/astaxie/beego" 
 
 type MainController struct{
 	beego.Controller
