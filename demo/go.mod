65d55aba8176d4d6d7de933b05149759a5d7e912
ShuwenHe <1201220707@pku.edu.cn>
2020-07-15 11:01:23 +0800

    fix: conntroller-get

---
+++ demo/go.mod
@@ -0,0 +1,5 @@
+module demo-beego/src/demo
+
+go 1.13
+
+require github.com/astaxie/beego v1.12.2
--- demo/main.go
+++ demo/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	_ "beego/code/demo/routers"
 	"github.com/astaxie/beego"
 )
 
 func main() {
 	beego.Run()
 }
-
